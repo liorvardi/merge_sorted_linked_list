391c5a82cc7e9ee727705b8542f57b8d2857959a
lior1 <lior1khtur@gmail.com>
2021-05-15 14:46:15 +0300

    updated

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <time.h>
+
 //
 // Created by lior1 on 5/6/2021.
 //
@@ -18,9 +20,6 @@ typedef enum {
     NULL_ARGUMENT,
 } ErrorCode;
 
-int getListLength(Node list) {
-    return 1;
-}
 
 bool isListSorted(Node list) {
     return true;
@@ -38,15 +37,17 @@ ErrorCode copy_list(Node source, Node dest) {
     if(dest == NULL){
         return NULL_ARGUMENT;
     }
-    while (source != NULL) {
+    while (source->next != NULL) {
+        dest->x = source->x;
         dest->next = malloc(sizeof(struct node_t));
         if ((dest->next) == NULL) {
             return MEMORY_ERROR;
         }
         dest = dest->next;
-        dest->x = source->x;
         source = source->next;
     }
+    dest->x = source->x;
+    dest->next = NULL;
     return SUCCESS;
 }
 
@@ -62,16 +63,16 @@ Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code) {
         *error_code = UNSORTED_LIST;
         return NULL;
     }
-    Node demi_node = malloc(sizeof(struct node_t));
-    if ((demi_node) == NULL) {
+    Node first_node = malloc(sizeof(struct node_t));
+    if ((first_node) == NULL) {
         *error_code = MEMORY_ERROR;
         return NULL;
     }
     Node merged_list;
-    for(merged_list = demi_node; list1 != NULL && list2 != NULL; merged_list = merged_list->next){
+    for(merged_list = first_node; list1 != NULL && list2 != NULL; merged_list = merged_list->next){
         merged_list->next = malloc(sizeof(struct node_t));
         if (NULL == (merged_list->next)) {
-            destroyList(demi_node);
+            destroyList(first_node);
             *error_code = MEMORY_ERROR;
             return NULL;
         }
@@ -92,26 +93,42 @@ Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code) {
         list_copy_error_code = copy_list(list1, merged_list);
     }
     if (list_copy_error_code == MEMORY_ERROR) {
-        destroyList(demi_node);
         *error_code = MEMORY_ERROR;
         return NULL;
     }
     assert(list_copy_error_code != NULL_ARGUMENT);
-    Node first_node = demi_node->next;
-    free(demi_node);
     return first_node;
 }
 
+
+void printList(Node list){
+    for(; list != NULL; list = list->next){
+        printf("%d ", list->x);
+    }
+    printf("\n");
+}
 int main() {
     Node list1 = malloc(sizeof(struct node_t));
     Node list2 = malloc(sizeof(struct node_t));
-    ErrorCode error_code;
-    error_code = SUCCESS;
-    list1->x = 2;
-    list1->next = NULL;
-    list2->x = 1;
-    list2->next = NULL;
-    Node merged = mergeSortedLists(list1, list2, &error_code);
-    printf("error code = %d\n2nd number = %d", error_code, merged->next->x);
+    Node first1 = list1;
+    Node first2 = list2;
+    list1->x = 4;
+    list2->x = 2;
+    for(int i = 0; i < 20; i++) {
+        list1->next = malloc(sizeof(*list1));
+        list2->next = malloc(sizeof(struct node_t));
+        list1->next->x=list1->x + 1;
+        list2->next->x = list2->x +2;
+        if(i<19){
+            list1 = list1->next;
+            list2 = list2->next;
+        }
+    }
+    list1->next = list2->next = NULL;
+    printList(first1);
+    printList(first2);
+    ErrorCode error;
+    Node merged = mergeSortedLists(first1, first2, &error);
+    printList(merged);
     return 0;
 }
